1f1434c718e3e3227a7fe9781da7c613f2146e45
mgm3IU <mgm3@indiana.edu>
2018-11-30 16:29:26 -0700

    Add files via upload

---
+++ project-code/main.py
@@ -0,0 +1,15 @@
+import random
+from timeit import default_timer as timer
+from classdef import Testing_List      
+        
+def main():
+    random.seed(1)
+    val_dict = {"one_hundred_rns" : Testing_List("test",[random.random()for i in range(100)])} 
+    val_dict.update({"one_thousand_rns" : Testing_List("test",[random.random()for i in range(1000)])})
+    val_dict.update({"ten_thousand_rns" : Testing_List("test",[random.random()for i in range(10000)])})
+    val_dict.update({"one_hun_thou_rns" : Testing_List("test",[random.random()for i in range(100000)])})
+    
+    
+    
+if __name__ == "__main__":
+    main()
